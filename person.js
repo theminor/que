7d5db562365ccd06c6f25101eecafb10244c0d20
John Minor <theminor@gmail.com>
2016-02-14 12:18:34 +0000

    ccs and playing

--- person.js
+++ person.js
@@ -1,8 +1,15 @@
-angular.module('nameApp', [])
+var nmApp = angular.module('nameApp', []);
 
-   .controller('nameController', function() {
-      // code
-   })
+nmApp.controller('NameController', ['$scope', function($scope) {
+   $scope.name = {
+      getFull: function() {
+         return ( ($scope.name.first || '') +
+                  ($scope.name.middle ? (' ' + $scope.name.middle) : '') +
+                  ($scope.name.last ? (' ' + $scope.name.last) : '') +
+                  ($scope.name.suffix ? (', ' + $scope.name.suffix) : '')
+               );
+      }
+   };
 
 
-;
+}]);
